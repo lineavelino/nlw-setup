5ba7a8958e756c23417d78fbdd2536d0d7a7cdc7
lineavelino <aline.savelino@gmail.com>
2023-01-19 05:11:07 -0300

    aula 2 mobile

---
+++ mobile/src/screens/Home.tsx
@@ -0,0 +1,56 @@
+import { ScrollView, Text, View } from "react-native";
+import { DAY_SIZE, HabitDay } from "../components/HabitDay";
+import { Header } from "../components/Header";
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearStart =
+  generateRangeDatesFromYearStart();
+const minimumSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill =
+  minimumSummaryDatesSizes - datesFromYearStart.length;
+
+export function Home() {
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <Header />
+
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, index) => (
+          <Text
+            key={`${weekDay}-${index}`}
+            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            style={{ width: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map(
+              (_, index) => (
+                <View
+                  key={index}
+                  className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                  style={{
+                    width: DAY_SIZE,
+                    height: DAY_SIZE,
+                  }}
+                />
+              )
+            )}
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
